9d890a6e4150094bad751dfdf5e04a58987b1432
lgrams <laurenrgrams@gmail.com>
2022-12-19 14:20:43 -0500

    completed properties of flight model

--- models/flight.js
+++ models/flight.js
@@ -2,10 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date
+    airline: {
+        type: String,
+        enum: 'American Southwest' || 'United'
+    },
+    airport: {
+        type: String,
+        enum: 'AUS' || 'DFW' || 'DEN' || 'LAX' || 'SAN',
+        default: 'DEN'
+    },
+    flightNo:{
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: date.setFullYear(date.getFullYear() + 1)
+    }
 });
 
 module.exports = mongoose.model('Flight', flightSchema);
